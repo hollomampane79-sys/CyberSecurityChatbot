b38c682b884b858edd3d30dff00ebf170eb3ac7a
Hollo Mampane <ST10184131@myemeris.edu.za>
2026-05-14 06:14:22 +0200

    Add expanded User model with memory, sentiment and favourite topic tracking

---
+++ PART 2/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CyberSecurityChatbot.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string FavouriteTopic { get; set; }
+        public string CurrentSentiment { get; set; } = "neutral";
+        public List<string> TopicsDiscussed { get; set; } = new List<string>();
+
+        public bool HasName => !string.IsNullOrWhiteSpace(Name);
+        public bool HasFavouriteTopic => !string.IsNullOrWhiteSpace(FavouriteTopic);
+    }
+}
